14e09060a8d9c0a50dc083d7f5019204aabb6a51
Rishabh Sharma <99526730+sharma-rishabh@users.noreply.github.com>
2024-09-29 13:46:30 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import { Link, Route, Routes, useParams } from 'react-router-dom';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+function TodoItem({ todo }) { 
+  return (
+    <div>
+      <h1>{todo.title}</h1>
+      <p>{todo.description}</p>
+    </div>
+  );
+}
+
+function Todo() {
+  const {id} = useParams()
+  const [todo, setTodo] = useState(null);
+  useEffect(() => {
+    fetch(`/api/todo/${id-1}`).then(res => res.json()).then(data => {
+      setTodo(data);
+    })
+  }, [id]);
+  return ( 
+    <div>
+      {todo ? <TodoItem todo={todo}/> : <div>Loading...</div>}
+    </div>
+  );
+
+}
+
+
+function TodoApp() { 
+  const [todos, setTodos] = useState([]);
+  
+  useEffect(() => {
+    fetch('/api/all-todos').then(res => res.json()).then(data => {
+      setTodos(data);
+    })
+  }, []);
+  return (
+    <div>
+      <h1>Todo List</h1>
+      {
+        todos.map(todo => {
+          return (
+            <Link to={`/todo/${todo.id}`}><div>{todo.id} {todo.title}</div></Link>
+          );
+        })
+      }
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<TodoApp />} />
+      <Route path="/todo/:id" element={<Todo />} />
+    </Routes>
+    // <TodoApp />
   );
 }
 
